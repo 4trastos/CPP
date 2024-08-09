3d06d484260d57d35a690d2679d42c3801ff845c
4trastos <93524280+4trastos@users.noreply.github.com>
2024-08-09 17:32:55 +0200

    OK

--- Module_00/ex00/Megaphone.cpp
+++ Module_00/ex00/Megaphone.cpp
@@ -18,7 +18,7 @@ int	main(int argc, char **argv)
 	int	j;
 
 	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE * 😏" << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE 😏 *" << std::endl;
 	else
 	{
 		i = 1;
