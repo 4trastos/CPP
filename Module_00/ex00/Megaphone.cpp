cf3f41489c57130508eeddb122cfb1e46d38b878
4trastos <93524280+4trastos@users.noreply.github.com>
2024-08-09 17:22:21 +0200

    OK

--- Module_00/ex00/Megaphone.cpp
+++ Module_00/ex00/Megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: davgalle <davgalle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 09:54:00 by davgalle          #+#    #+#             */
-/*   Updated: 2024/08/09 10:54:23 by davgalle         ###   ########.fr       */
+/*   Updated: 2024/08/09 17:13:20 by davgalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,25 @@
 
 int	main(int argc, char **argv)
 {
-	
+	int	i;
+	int	j;
+
+	if (argc == 1)
+		std::cout << "I need arguments... up to you 😏" << std::endl;
+	else
+	{
+		i = 1;
+		while (argv[i] != NULL)
+		{
+			j = 0;
+			while (argv[i][j] != '\0')
+			{
+				std::cout << (char)toupper(argv[i][j]);
+				j++;
+			}
+			i++;
+		}		
+	}
+	std::cout << std::endl;
 	return (0);
 }
--- Module_00/ex00/Megaphone.hpp
+++ Module_00/ex00/Megaphone.hpp
@@ -6,7 +6,16 @@
 /*   By: davgalle <davgalle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 10:54:42 by davgalle          #+#    #+#             */
-/*   Updated: 2024/08/09 10:54:57 by davgalle         ###   ########.fr       */
+/*   Updated: 2024/08/09 11:01:54 by davgalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MEGAPHONE_HPP
+# define MEGAPHONE_HPP
+
+# include <stdio.h>
+# include <iostream>
+# include <string>
+# include <locale>
+
+#endif
