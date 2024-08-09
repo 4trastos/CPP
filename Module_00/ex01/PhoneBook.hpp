023bd67947157c64034ac9f6f4ca90ebf2b669a2
4trastos <93524280+4trastos@users.noreply.github.com>
2024-08-09 17:50:19 +0200

    Add files via upload

---
+++ Module_00/ex01/PhoneBook.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davgalle <davgalle@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/09 17:37:12 by davgalle          #+#    #+#             */
+/*   Updated: 2024/08/09 17:46:29 by davgalle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_H
+# define PHONEBOOK_H
+
+# include <string>
+# include <stdio.h>
+# include <iostream>
+# include <locale>
+
+
+#endif
