de5c4115671da57430300c87b720b762084c4238
4trastos <93524280+4trastos@users.noreply.github.com>
2024-08-16 12:34:08 +0200

    ok

--- Module_00/ex00/Megaphone.cpp
+++ Module_00/ex00/Megaphone.cpp
@@ -10,8 +10,38 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Megaphone.hpp"
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <locale>
 
+int	main(int argc, char **argv)
+{
+	std::string str;
+	int	i;
+	int	j;
+
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		i = 1;
+		while (argv[i] != NULL)
+		{
+			j = 0;
+			str = argv[i];
+			while (str[j] != '\0')
+			{
+				str[j] = toupper(str[j]);
+				j++;
+			}
+			std::cout << str;
+			i++;
+		}		
+	}
+	std::cout << std::endl;
+	return (0);
+}
 int	main(int argc, char **argv)
 {
 	std::string str;
