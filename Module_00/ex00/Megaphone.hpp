46256ffb75fda186737eda7ce9064f1fe4473c90
4trastos <93524280+4trastos@users.noreply.github.com>
2024-08-16 12:32:31 +0200

    OK

--- Module_00/ex00/Megaphone.hpp
+++ Module_00/ex00/Megaphone.hpp
@@ -1,41 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Megaphone.cpp                                      :+:      :+:    :+:   */
+/*   Megaphone.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: davgalle <davgalle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/09 09:54:00 by davgalle          #+#    #+#             */
-/*   Updated: 2024/08/16 12:30:54 by davgalle         ###   ########.fr       */
+/*   Created: 2024/08/09 10:54:42 by davgalle          #+#    #+#             */
+/*   Updated: 2024/08/09 11:01:54 by davgalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Megaphone.hpp"
+#ifndef MEGAPHONE_HPP
+# define MEGAPHONE_HPP
 
-int	main(int argc, char **argv)
-{
-	std::string str;
-	int	i;
-	int	j;
+# include <stdio.h>
+# include <iostream>
+# include <string>
+# include <locale>
 
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else
-	{
-		i = 1;
-		while (argv[i] != NULL)
-		{
-			j = 0;
-			str = argv[i];
-			while (str[j] != '\0')
-			{
-				str[j] = toupper(str[j]);
-				j++;
-			}
-			std::cout << str;
-			i++;
-		}		
-	}
-	std::cout << std::endl;
-	return (0);
-}
+#endif
