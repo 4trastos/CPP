749ee4248769c29daf42ee389972fc8425fa8d77
4trastos <93524280+4trastos@users.noreply.github.com>
2024-08-21 09:27:37 +0200

    Perfect!

--- Module_01/ex04/main.cpp
+++ Module_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: davgalle <davgalle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 15:59:03 by davgalle          #+#    #+#             */
-/*   Updated: 2024/08/20 16:20:18 by davgalle         ###   ########.fr       */
+/*   Updated: 2024/08/21 08:41:07 by davgalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,50 @@ int	emptyString(std::string s1, std::string s2)
 	return (0);
 }
 
+void	copy(std::string s1, std::string s2, std::ifstream &file_in, std::ofstream &file_out)
+{
+	std::string str;
+
+	while (getline(file_in, str))
+	{
+		std::size_t find_pos;
+		while ((find_pos = str.find(s1)) != std::string::npos)
+		{
+			file_out << str.substr(0, find_pos) << s2;
+			str.erase(0, find_pos + s1.length());
+		}
+		file_out << str << std::endl;
+	}
+}
+
 void	replace(std::string file, std::string s1, std::string s2)
 {
 	if (emptyString(s1, s2))
 		exit(1);
-	(void)file;
+	
+	std::ifstream file_in;
+
+	file_in.open(file.c_str());
+	if (!file_in.is_open())
+	{
+		std::cout << "Error: " << file << " do not open" << std::endl;
+		exit (1);
+	}
+	file += ".replace";
+
+	std::ofstream file_out;
+
+	file_out.open(file.c_str());
+	if (!file_out.is_open())
+	{
+		std::cout << "Error: " << file << " do not open" << std::endl;
+		exit (1);
+	}
+
+	copy(s1, s2, file_in, file_out);
+
+	file_in.close();
+	file_out.close();
 }
 
 int main(int argc, char **argv)
