29c0afc37f411c91900b8c0bdea6cacddb68952b
4trastos <93524280+4trastos@users.noreply.github.com>
2024-08-22 11:43:59 +0200

    zombieHorde.cpp

--- Module_01/ex01/zombieHorde.cpp
+++ Module_01/ex01/zombieHorde.cpp
@@ -16,9 +16,6 @@ Zombie* zombieHorde(int N, std::string name)
 {
 	Zombie* zombieLegio = new Zombie[N];
 	for (int i = 0; i < N; i++)
-	{
-		std::string newName = name;
 		zombieLegio[i].setName(newName);
-	}
 	return (zombieLegio);
 }
