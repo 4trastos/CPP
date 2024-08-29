e0957dfb3baa3c4cf95106ad0eb8f4518589450f
4trastos <93524280+4trastos@users.noreply.github.com>
2024-08-30 00:06:18 +0200

    Add files via upload

---
+++ Module_02/ex00/Fixed.cpp
@@ -0,0 +1,37 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+    std::cout << "Default constructor called" << std::endl;
+    this->value = 0;
+}
+
+Fixed::Fixed(const Fixed& copy)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = copy;
+}
+
+Fixed& Fixed::operator = (const Fixed& copy)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    this->value = copy.getRawBits();
+    return (*this);
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;    
+}
+
+void    Fixed::setRawBits(int const raw)
+{
+    std::cout << "setRawBits member function called" << std::endl;
+    this->value = raw;
+}
+
+int Fixed::getRawBits(void) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return (this->value);
+}
---
+++ Module_02/ex00/Fixed.hpp
@@ -0,0 +1,25 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <string>
+# include <iostream>
+# include <iomanip>
+
+class Fixed
+{
+    private:
+        int value;
+        static const int BITS = 8;
+    public:
+        Fixed();
+
+        Fixed(const Fixed& copy);
+        Fixed& operator = (const Fixed& copy);
+
+        ~Fixed();
+
+        void setRawBits(int const raw);
+        int getRawBits(void) const;
+};
+
+#endif
